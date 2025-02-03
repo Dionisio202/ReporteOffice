d91692eafa0ec9b52df9d2f59990512c166bd41b
Karen Moyolema <kmoyolema3943@uta.edu.ec>
2025-02-03 11:21:20 -0500

    Formularios

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,8 @@
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 import Header from "./components/header/Header";
 import Footer from "./components/footer/Footer";
+import Formulario3 from "./components/formularios/formulario3";
+import Formulario4 from "./components/formularios/formulario4";
 import GestorReportes from './pages/gestor_reportes/gestor_reportes';
 import ReporteEditor from './pages/reporte_editor/reporte_editor';
 import GestorDocumentos from './pages/gestor_documentos/gestor_documentos';
@@ -21,7 +23,9 @@ const App = () => {
         <Route path="/GestorReporte" element={<GestorReportes />} />
         <Route path="/Principal" element={<Principal />} />
         <Route path="/Formato" element={<Format />} />
-      </Routes>
+        <Route path="/Formulario3" element={<Formulario3 />} />
+        <Route path="/Formulario4" element={<Formulario4 />} />
+        </Routes>
     </Router>
     </main>
       <Footer />
